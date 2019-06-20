4aabd0313c0596d72e8d1e69f29598f14d7d065c
DUQUE-PC\danyd <danyduque9@hotmail.com>
2019-06-20 18:25:24 +0200

    Login activity

---
+++ app/src/main/java/com/dani/dialogflowassistant/vista/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.dani.dialogflowassistant.vista;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.dani.dialogflowassistant.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
